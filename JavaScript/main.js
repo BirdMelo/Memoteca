c1b43d467e252a18cbcbef993f2e2be6f07aebb1
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2024-12-20 13:56:37 -0300

    06. Evitando pensamentos duplicados

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,17 +1,39 @@
 import api from "./api.js"
 import ui from "./ui.js"
 
-const regexContent = /^[A-Za-z\s]{10,259}$/
-const regexAutoria = /^[A-Za-z_]{3,15}$/
+const regexContent = /^.{10,259}$/
+const regexAutoria = /^[A-Za-z\s]{3,15}$/
+const thoughtsSet = new Set()
+
+async function addThoughtsKey() {
+    try {
+        const thoughts = await api.searchThoughts()
+        thoughts.forEach(thought => {
+            const key = `${thought.conteudo.trim().toLowerCase()}-${thought.autoria.trim().toLowerCase()}`
+            thoughtsSet.add(key)
+        })
+    } catch (error) {
+        alert('Erro ao adicinar chave')
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.renderizeThought()
+    addThoughtsKey()
     const thoughtForm = document.querySelector('#pensamento-form')
     thoughtForm.addEventListener('submit', manipulateSubmit)
     const cancelForm = document.querySelector('#botao-cancelar')
     cancelForm.addEventListener('click', manipulateCancel)
     const inputSearch = document.querySelector('#campo-busca')
     inputSearch.addEventListener('input', manipulateSearch)
+
+    const textarea = document.querySelector('#pensamento-conteudo')
+    const countCaracterTextarea = document.querySelector('#caracterCountTextArea')
+    textarea.addEventListener('input',() => { updateCount(textarea, countCaracterTextarea, 150)})
+
+    const autoria = document.querySelector('#pensamento-autoria')
+    const countCaracterAutoria = document.querySelector('#caracterCountAutoria')
+    autoria.addEventListener('input', () => {updateCount(autoria, countCaracterAutoria, 15)})
 })
 
 async function manipulateSubmit(event) {
@@ -21,21 +43,41 @@ async function manipulateSubmit(event) {
     const autoria = document.querySelector('#pensamento-autoria').value.trim()
     const data = document.querySelector('#pensamento-data').value
 
+    const textarea = document.querySelector('#pensamento-conteudo')
+    const errorTextarea = document.querySelector('#textareaLabel p:nth-child(2)')
+    const inputAutoria = document.querySelector('#pensamento-autoria')
+    const errorAutoria = document.querySelector('#autoriaLabel p:nth-child(2)')
+
     if(!validateText(regexContent, conteudo)){
-        alert("Pensamento deve ter letras maiúsculas e/ou minúsculas, espaço, não será salvo um pensamento com apenas espaços, no mínimo 10 caracteres e no máximo 259.")
-        return
+        textarea.style.border = 'solid 1px red'
+        errorTextarea.style.diplay = 'block'
+        errorTextarea.textContent = "*Pensamento deve conter entre 10 e 150 caracteres. Espaços em volta do texto e texto com apenas espaços serão desconsiderados."
     }
     if(!validateText(regexAutoria, autoria)) {
-        alert("Deve ter a penas letras em um espaço de no minimo 3 caracteres e no máximo 15 caractes. Caso o nome do autor for grande priorise primeiro e ultimo nome com um espaçamento com '_'(Underline)")
-        return
+        inputAutoria.style.border = 'solid 1px red'
+        errorAutoria.style.display = 'block'
+        errorAutoria.textContent = "*Autoria deve conter: entre 3 a 15 caracteres, sem números e/ou caracteres especiais. Espaços em volta do texto e texto com apenas espaços serão desconsiderados."
     }
 
     if(!verificateDate(data)) {
         alert('Não é permitido datas futuras')
+    }
+    if(!validateText(regexContent, conteudo) || !validateText(regexAutoria, autoria) || !verificateDate(data)) {
+        return
+    }
+
+    const newThoughtKey = `${conteudo.trim().toLowerCase()}-${autoria.trim().toLowerCase()}`
+    if(thoughtsSet.has(newThoughtKey)) {
+        alert('Pensamento já cadastrado')
         return
     }
 
     try {
+        textarea.style.border = 'none'
+        errorTextarea.style.diplay = 'none'
+        inputAutoria.style.border = 'none'
+        errorAutoria.style.diplay = 'none'
+        
         if(id){
             await api.editeThought({ id, conteudo, autoria, data })
         }else {
@@ -67,4 +109,10 @@ function verificateDate(data) {
 }
 function validateText(regex,text) {
     return regex.test(text)
+}
+function updateCount(inputElement, counter, limit) {
+    const count = inputElement.value.length;
+    counter.textContent = `${count}/${limit}`
+
+    counter.style.color = count > limit ? 'red' : '#8F8F8F';
 }
